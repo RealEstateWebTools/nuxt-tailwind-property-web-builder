3037573c24b824c63aa3d3d8334b115da927f0e2
Ed Tee <etewiah@gmail.com>
2021-07-21 20:04:15 +0100

    Add hello world page

---
+++ pages/hello.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="pwb-root">
+    <div class="v-application v-application--is-ltr theme--light">
+      <div class="pwb-p-site v-application--wrap w-full top-nav-admin-hoist">
+        <div class="flex flex-col min-h-screen">
+          <h1>Hello World</h1>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import PageFooter from "~/components/pwb/PageFooter"
+import PageHeader from "~/components/pwb/PageHeader"
+import PageMain from "~/components/pwb/PageMain"
+export default {
+  components: {
+    PageFooter,
+    PageHeader,
+    PageMain,
+  },
+}
+</script>
