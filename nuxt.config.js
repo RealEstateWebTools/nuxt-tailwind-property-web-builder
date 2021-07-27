566f30afa43cd0ed0d3029b8d08a905e21344f3e
Ed Tee <etewiah@gmail.com>
2021-07-27 12:55:16 +0100

    Add vuetify

--- nuxt.config.js
+++ nuxt.config.js
@@ -51,7 +51,16 @@ export default {
     // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
     buildModules: [
         // https://go.nuxtjs.dev/tailwindcss
-        "@nuxtjs/tailwindcss"
+        "@nuxtjs/tailwindcss",
+        // Simple usage
+        // "@nuxtjs/vuetify",
+        // With options
+        [
+            "@nuxtjs/vuetify",
+            {
+                /* module options */
+            }
+        ]
     ],
 
     // Modules: https://go.nuxtjs.dev/config-modules
