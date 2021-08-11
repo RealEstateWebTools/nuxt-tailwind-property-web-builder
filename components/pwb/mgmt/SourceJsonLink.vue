0cbb07e15ea0d2da5c3f07ab85f9cdfef7a3472c
Ed Tee <etewiah@gmail.com>
2021-08-11 19:05:03 +0100

    Add SourceJsonLink component to allow devs to debug json data

---
+++ components/pwb/mgmt/SourceJsonLink.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="source-json-link">
+    {{currComponentName}}: 
+    <a :href="sourceJsonUrl" target="__blank">Show source JSON</a>
+    <button @click="refreshSourceJson">Refresh source JSON</button>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    sourceJsonUrl: {
+      type: String,
+      default: "/",
+    },
+    currComponentName: {}
+  },
+  methods: {
+    refreshSourceJson() {
+      this.$emit("refreshSourceJson")
+    },
+  },
+}
+</script>
